7b37b42d1087ff9d82f72c0abab96b2ca52ee810
Fatema-Nawrin <nawrin4000@gmail.com>
2024-12-09 02:58:44 +0600

    day8 added

--- 2024/Day 8/day8.py
+++ 2024/Day 8/day8.py
@@ -17,14 +17,14 @@ def part1(data):
     rows, cols, antennas = parse_input(data)
     antinodes = set()
     for antenna, positions in antennas.items():
-        for i, p1 in enumerate(list(positions)):
-            for p2 in list(positions)[i + 1 :]:
-                dis = (p2[0] - p1[0], p2[1] - p1[1])
-                antinode1 = (p1[0] - dis[0], p1[1] - dis[1])
-                antinode2 = (p2[0] + dis[0], p2[1] + dis[1])
-                for pos in [antinode1, antinode2]:
-                    if 0 <= pos[0] < rows and 0 <= pos[1] < cols:
-                        antinodes.add(pos)
+        for i, a in enumerate(list(positions)):
+            for b in list(positions)[i + 1 :]:
+                dis = (b[0] - a[0], b[1] - a[1])
+                an1 = (a[0] - dis[0], a[1] - dis[1])
+                an2 = (b[0] + dis[0], b[1] + dis[1])
+                for p in [an1, an2]:
+                    if 0 <= p[0] < rows and 0 <= p[1] < cols:
+                        antinodes.add(p)
 
     return len(antinodes)
 
@@ -33,22 +33,23 @@ def part2(data):
     rows, cols, antennas = parse_input(data)
     antinodes = set()
     for antenna, positions in antennas.items():
-        for i, p1 in enumerate(list(positions)):
-            for p2 in list(positions)[i + 1 :]:
-                dis = (p2[0] - p1[0], p2[1] - p1[1])
-                antinode1 = p1
-                antinode2 = p2
-                while (0 <= antinode1[0] < rows and 0 <= antinode1[1] < cols) or (
-                    0 <= antinode2[0] < rows and 0 <= antinode2[1] < cols
+        for i, a in enumerate(list(positions)):
+            for b in list(positions)[i + 1 :]:
+                dis = (b[0] - a[0], b[1] - a[1])
+                an1 = a
+                an2 = b
+                while (0 <= an1[0] < rows and 0 <= an1[1] < cols) or (
+                    0 <= an2[0] < rows and 0 <= an2[1] < cols
                 ):
-                    if 0 <= antinode1[0] < rows and 0 <= antinode1[1] < cols:
-                        antinodes.add(antinode1)
-                    if 0 <= antinode2[0] < rows and 0 <= antinode2[1] < cols:
-                        antinodes.add(antinode2)
-                    antinode1 = (antinode1[0] - dis[0], antinode1[1] - dis[1])
-                    antinode2 = (antinode2[0] + dis[0], antinode2[1] + dis[1])
+                    if 0 <= an1[0] < rows and 0 <= an1[1] < cols:
+                        antinodes.add(an1)
+                    if 0 <= an2[0] < rows and 0 <= an2[1] < cols:
+                        antinodes.add(an2)
+                    an1 = (an1[0] - dis[0], an1[1] - dis[1])
+                    an2 = (an2[0] + dis[0], an2[1] + dis[1])
 
     return len(antinodes)
 
 
+print(part1(data))
 print(part2(data))
